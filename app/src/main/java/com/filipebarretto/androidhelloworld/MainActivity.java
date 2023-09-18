2acf127caffa935cf72e4b3ba121bcfa3de432e4
Uzair Mansoor <mansooru@NETSOLPK.COM>
2023-09-18 14:41:30 +0500

    changes

--- app/src/main/java/com/filipebarretto/androidhelloworld/MainActivity.java
+++ app/src/main/java/com/filipebarretto/androidhelloworld/MainActivity.java
@@ -1,7 +1,7 @@
 package com.filipebarretto.androidhelloworld;
 
 // import android.support.v7.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppcompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
